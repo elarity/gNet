ea4cdd613c81188b239852e5ebf3d8b67cbfaf69
elarity <42677833@qq.com>
2023-04-15 12:47:12 +0800

    测试代码完善

--- test/client.go
+++ test/client.go
@@ -3,29 +3,34 @@ package main
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("tcp-client net.Dial")
-	clientTcpConn, err := net.Dial("tcp", "127.0.0.1:9191")
-	if err != nil {
-		fmt.Printf("net.Dial err=%+v", err)
-		return
-	}
-	writeContentLength, err := clientTcpConn.Write([]byte("hello-world-hello-world"))
-	if err != nil {
-		fmt.Printf("clientTcpConn.Write err=%+v", err)
-		return
-	}
-	buffer := make([]byte, 512)
-	fmt.Println("tcp-client net.Dial Read")
-	readContentLenth, err := clientTcpConn.Read(buffer)
-	fmt.Println("tcp-client net.Dial Read Over")
-	if err != nil {
-		fmt.Printf("clientTcpConn.Read error=%+v", err)
-		return
-	}
-	fmt.Printf("Message From Svr:%s, readContentLenth=%d, writeContentLength=%d\n", buffer, readContentLenth, writeContentLength)
+	for {
 
+		fmt.Println("tcp-client net.Dial")
+		clientTcpConn, err := net.Dial("tcp", "127.0.0.1:9191")
+		if err != nil {
+			fmt.Printf("net.Dial err=%+v", err)
+			return
+		}
+		writeContentLength, err := clientTcpConn.Write([]byte("hello-world-hello-world"))
+		if err != nil {
+			fmt.Printf("clientTcpConn.Write err=%+v", err)
+			return
+		}
+		buffer := make([]byte, 5120)
+		fmt.Println("tcp-client net.Dial Read")
+		readContentLenth, err := clientTcpConn.Read(buffer)
+		fmt.Println("tcp-client net.Dial Read Over")
+		if err != nil {
+			fmt.Printf("clientTcpConn.Read error=%+v", err)
+			return
+		}
+		fmt.Printf("Message From Svr:%s, readContentLenth=%d, writeContentLength=%d\n", buffer, readContentLenth, writeContentLength)
+
+		time.Sleep(1 * time.Second)
+	}
 }
--- test/svr.go
+++ test/svr.go
@@ -1,12 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"github.com/elarity/gNet/core"
+	"github.com/elarity/gNet/iface"
 )
 
 func main() {
-
+	core.InitConf()
 	svr := core.InitServer()
+	svr.AddRouter(&TestRouter{})
 	svr.Serv()
 
+}
+
+type TestRouter struct {
+	iface.Router
+}
+
+func (r *TestRouter) HandlerBefore(request iface.Request) {
+	fmt.Println("HandlerBefore")
+	_, err := request.GetConn().GetRawTcpConnFd().Write([]byte("HandlerBefore fire\n"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func (r *TestRouter) HandlerFire(request iface.Request) {
+	fmt.Println("HandlerFire")
+	_, err := request.GetConn().GetRawTcpConnFd().Write([]byte("HandlerFire fire\n"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func (r *TestRouter) HandlerAfter(request iface.Request) {
+	fmt.Println("HandlerAfter")
+	_, err := request.GetConn().GetRawTcpConnFd().Write([]byte("HandlerAfter fire\n"))
+	if err != nil {
+		fmt.Println(err)
+	}
 }
